26f640713d0e4f1ea51ff59e8cf127e64b872d6b
piEsposito <piero.skywalker@gmail.com>
2020-03-02 11:24:13 -0300

    added sequential model test - passing

--- bbp_torch/losses/kl_divergence.py
+++ bbp_torch/losses/kl_divergence.py
@@ -5,6 +5,11 @@ import torch.nn.functional as F
 from bbp_torch.modules.base_bayesian_module import BayesianModule
 
 def kl_divergence_from_nn(model):
+
+    """
+    Gathers the KL Divergence from a nn.Module object
+    Works by gathering each Bayesian layer kl divergence and summing it, doing nothing with the non Bayesian ones
+    """
     kl_divergence = 0
     for module in model.modules():
         if isinstance(module, (BayesianModule)):
