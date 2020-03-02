333f2cb1de5d9825acf63e231d3e04ac656af257
piEsposito <piero.skywalker@gmail.com>
2020-03-02 11:17:18 -0300

    kl_divergence from module working

--- bbp_torch/losses/kl_divergence.py
+++ bbp_torch/losses/kl_divergence.py
@@ -1,3 +1,13 @@
 import torch
 import torch.nn
-import torch.nn.functional as F    
+import torch.nn.functional as F
+
+from bbp_torch.modules.base_bayesian_module import BayesianModule
+
+def kl_divergence_from_nn(model):
+    kl_divergence = 0
+    for module in model.modules():
+        if isinstance(module, (BayesianModule)):
+            kl_divergence += module.log_variational_posterior - module.log_prior
+    return kl_divergence
+
