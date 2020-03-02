08a7bf36d729438242389da9f29ddc8f8ba1dff9
piEsposito <piero.skywalker@gmail.com>
2020-03-02 10:33:24 -0300

    added mid-level bayesian module class no enable specific behavior

---
+++ bbp_torch/modules/base_bayesian_module_test.py
@@ -0,0 +1,15 @@
+import unittest
+import torch
+from torch import nn
+
+from base_bayesian_module import BayesianModule
+
+class TestLinearBayesian(unittest.TestCase):
+
+    def test_init_bayesian_layer(self):
+        b_module = BayesianModule()
+        self.assertEqual(isinstance(b_module, (nn.Module)), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
