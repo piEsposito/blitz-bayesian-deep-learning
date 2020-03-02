e71e7d5b07fdf2e07f10f37889642c59f44b3c2a
piEsposito <piero.skywalker@gmail.com>
2020-03-02 14:08:37 -0300

    conv2d layer working

--- bbp_torch/modules/conv2d_bayesian_layer_test.py
+++ bbp_torch/modules/conv2d_bayesian_layer_test.py
@@ -1,14 +1,33 @@
 import unittest
+from bbp_torch.modules import BayesianConv2d
+
+import torch
+from torch import nn
 
 class TestConv2DBayesian(unittest.TestCase):
 
     def test_init_bayesian_layer(self):
         #create bayesian layer
+
+        module = BayesianConv2d(3, 10, (3,3))
         pass
 
     def test_weights_shape(self):
-        #check if weights shape is the expected
-        pass
+       #check if weights shape is the expected
+       to_feed = torch.ones((1, 3, 25, 25))
+       bconv = BayesianConv2d(in_channels=3,
+                              out_channels=3,
+                              kernel_size=(3,3))
+
+       conv = nn.Conv2d(in_channels=3,
+                        out_channels=3,
+                        kernel_size=(3,3))
+
+       infer1 = bconv(to_feed)
+       infer2 = conv(to_feed)
+       
+       self.assertEqual(infer1.shape, infer2.shape)
+       pass
 
     def test_variational_inference(self):
         #create module, check if inference is variating
