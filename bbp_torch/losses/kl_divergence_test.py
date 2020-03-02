3e51ca649852f0c7746436ac417d9602399d5b84
piEsposito <piero.skywalker@gmail.com>
2020-03-02 11:17:18 -0300

    kl_divergence from module working

--- bbp_torch/losses/kl_divergence_test.py
+++ bbp_torch/losses/kl_divergence_test.py
@@ -1,9 +1,25 @@
 import unittest
+import torch
+import torch.nn
+import torch.nn.functional as F
 
+from bbp_torch.losses import kl_divergence_from_nn
+from bbp_torch.modules import BayesianLinear
 
 class TestKLDivergence(unittest.TestCase):
 
-    def test_kl_divergence(self):
+    def test_kl_divergence_bayesian_module(self):
+        blinear = BayesianLinear(10, 10)
+        to_feed = torch.ones((1, 10))
+        predicted = blinear(to_feed)
+
+        complexity_cost = blinear.log_variational_posterior - blinear.log_prior
+        kl_complexity_cost = kl_divergence_from_nn(blinear)
+
+        self.assertEqual((complexity_cost == kl_complexity_cost).all(), torch.tensor(True))
+        pass
+    
+    def test_kl_divergence_non_bayesian_module(self):
         pass
 
     def test_kl_divergence_whole_model(self):
