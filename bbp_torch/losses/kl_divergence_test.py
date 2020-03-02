fa1763308118e508c55048d714180425ef9bee47
piEsposito <piero.skywalker@gmail.com>
2020-03-02 14:26:10 -0300

    kl divergence tests on conv2d passing

--- bbp_torch/losses/kl_divergence_test.py
+++ bbp_torch/losses/kl_divergence_test.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from bbp_torch.losses import kl_divergence_from_nn
-from bbp_torch.modules import BayesianLinear
+from bbp_torch.modules import BayesianLinear, BayesianConv2d
 
 class TestKLDivergence(unittest.TestCase):
 
-    def test_kl_divergence_bayesian_module(self):
+    def test_kl_divergence_bayesian_linear_module(self):
         blinear = BayesianLinear(10, 10)
         to_feed = torch.ones((1, 10))
         predicted = blinear(to_feed)
@@ -19,6 +19,20 @@ class TestKLDivergence(unittest.TestCase):
         self.assertEqual((complexity_cost == kl_complexity_cost).all(), torch.tensor(True))
         pass
     
+    def test_kl_divergence_bayesian_conv2d_module(self):
+        bconv = BayesianConv2d(in_channels=3,
+                              out_channels=3,
+                              kernel_size=(3,3))
+
+        to_feed = torch.ones((1, 3, 25, 25))
+        predicted = bconv(to_feed)
+        
+        complexity_cost = bconv.log_variational_posterior - bconv.log_prior
+        kl_complexity_cost = kl_divergence_from_nn(bconv)
+
+        self.assertEqual((complexity_cost == kl_complexity_cost).all(), torch.tensor(True))
+        pass
+
     def test_kl_divergence_non_bayesian_module(self):
         linear = nn.Linear(10, 10)
         to_feed = torch.ones((1, 10))
@@ -26,9 +40,6 @@ class TestKLDivergence(unittest.TestCase):
 
         kl_complexity_cost = kl_divergence_from_nn(linear)
         self.assertEqual((torch.tensor(0) == kl_complexity_cost).all(), torch.tensor(True))
-        pass
-
-    def test_kl_divergence_whole_model(self):
         pass
 
 if __name__ == "__main__":
--- bbp_torch/modules/conv2d_bayesian_layer_test.py
+++ bbp_torch/modules/conv2d_bayesian_layer_test.py
@@ -1,5 +1,6 @@
 import unittest
 from bbp_torch.modules import BayesianConv2d
+from bbp_torch.modules.base_bayesian_module import BayesianModule
 
 import torch
 from torch import nn
@@ -61,10 +62,30 @@ class TestConv2DBayesian(unittest.TestCase):
         self.assertEqual((bconv(to_feed) != bconv(to_feed)).any(), torch.tensor(True))
         self.assertEqual((bconv.forward_frozen(to_feed) == bconv.forward_frozen(to_feed)).all(), torch.tensor(True))
         pass
+    
+    def test_inheritance(self):
+
+        #check if bayesian linear has nn.Module and BayesianModule classes
+        bconv = BayesianConv2d(in_channels=3,
+                              out_channels=3,
+                              kernel_size=(3,3),
+                              bias=False)
+
+        self.assertEqual(isinstance(bconv, (nn.Module)), True)
+        self.assertEqual(isinstance(bconv, (BayesianModule)), True)
 
     def test_kl_divergence(self):
         #create model, sample weights
         #check if kl divergence between apriori and a posteriori is working
+        bconv = BayesianConv2d(in_channels=3,
+                              out_channels=3,
+                              kernel_size=(3,3))
+
+        to_feed = torch.ones((1, 3, 25, 25))
+        predicted = bconv(to_feed)
+        
+        complexity_cost = bconv.log_variational_posterior - bconv.log_prior
+        self.assertEqual((complexity_cost == complexity_cost).all(), torch.tensor(True))
         pass
 
 if __name__ == "__main__":
