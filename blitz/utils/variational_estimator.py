30866b7f3581126d8a865a22816816dabcd5d25e
piEsposito <piero.skywalker@gmail.com>
2020-03-03 11:08:46 -0300

    variational estimator test on elbo

--- blitz/utils/variational_estimator.py
+++ blitz/utils/variational_estimator.py
@@ -4,7 +4,25 @@ from blitz.losses import kl_divergence_from_nn
 def variational_estimator(nn_class):
 
     def nn_kl_divergence(self):
+        #returns the kl_divergence of the model, works for better encapsulation
+        #nothing new under the sun
         return kl_divergence_from_nn(self)
     
     setattr(nn_class, "nn_kl_divergence", nn_kl_divergence)
+
+    def sample_elbo(self,
+                    inputs,
+                    labels,
+                    criterion,
+                    sample_nbr):
+
+        loss = 0
+        for _ in range(sample_nbr):
+            outputs = self(inputs)
+            loss = criterion(outputs, labels)
+            loss += self.nn_kl_divergence()
+        return loss / sample_nbr
+    
+    setattr(nn_class, "sample_elbo", sample_elbo)
+
     return nn_class
--- blitz/utils/variational_estimator_test.py
+++ blitz/utils/variational_estimator_test.py
@@ -1,6 +1,9 @@
 import unittest
 import torch
 from torch import nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+
 from blitz.modules import BayesianConv2d, BayesianLinear
 from blitz.losses import kl_divergence_from_nn
 from blitz.utils import variational_estimator
@@ -31,9 +34,39 @@ class TestVariationalInference(unittest.TestCase):
         self.assertEqual((complexity_cost == kl_complexity_cost).all(), torch.tensor(True))
 
     def test_variance_over_prediction(self):
-        #create model
-        #do two inferences over model
-        #check if able to gather variance over predictions
+        dataset = dsets.MNIST(root="./data",
+                                    train=True,
+                                    transform=transforms.ToTensor(),
+                                    download=True
+                                    )
+
+        dataloader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                                   batch_size=16,
+                                                   shuffle=True)
+
+        batch = next(iter(dataloader))
+
+        @variational_estimator
+        class BayesianMLP(nn.Module):
+            def __init__(self, input_dim, output_dim):
+                super().__init__()
+                #self.linear = nn.Linear(input_dim, output_dim)
+                self.blinear1 = BayesianLinear(input_dim, 512)
+                self.blinear2 = BayesianLinear(512, output_dim)
+                
+            def forward(self, x):
+                x_ = x.view(-1, 28 * 28)
+                x_ = self.blinear1(x_)
+                return self.blinear2(x_)
+
+        net = BayesianMLP(28*28, 10)
+        elbo = net.sample_elbo(inputs=batch[0],
+                               labels=batch[1],
+                               criterion=torch.nn.CrossEntropyLoss(),
+                               sample_nbr=5)
+
+        self.assertEqual((elbo==elbo).all(), torch.tensor(True))
+        
         pass
 
     def test_freeze_estimator(self):
