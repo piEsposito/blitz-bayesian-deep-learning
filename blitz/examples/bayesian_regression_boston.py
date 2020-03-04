099cd0415a1716e96bee9fa17d52b6881417fd73
piEsposito <piero.skywalker@gmail.com>
2020-03-04 10:13:59 -0300

    sklearn on requirements and bayesian CI regression

---
+++ blitz/examples/bayesian_regression_boston.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+from blitz.modules import BayesianLinear
+from blitz.losses import kl_divergence_from_nn
+from blitz.utils import variational_estimator
+
+from sklearn.datasets import load_boston
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+X, y = load_boston(return_X_y=True)
+X = StandardScaler().fit_transform(X)
+y = StandardScaler().fit_transform(np.expand_dims(y, -1))
+
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=.25,
+                                                    random_state=42)
+
+
+X_train, y_train = torch.tensor(X_train).float(), torch.tensor(y_train).float()
+X_test, y_test = torch.tensor(X_test).float(), torch.tensor(y_test).float()
+
+
+@variational_estimator
+class BayesianRegressor(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        #self.linear = nn.Linear(input_dim, output_dim)
+        self.blinear1 = BayesianLinear(input_dim, 512)
+        self.blinear2 = BayesianLinear(512, output_dim)
+        
+    def forward(self, x):
+        x_ = self.blinear1(x)
+        return self.blinear2(x_)
+
+
+def evaluate_regression(regressor,
+                        X,
+                        y,
+                        samples = 100,
+                        std_multiplier = 2):
+    preds = [regressor(X) for i in range(samples)]
+    preds = torch.stack(preds)
+    means = preds.mean(axis=0)
+    stds = preds.std(axis=0)
+    ci_upper = means + (std_multiplier * stds)
+    ci_lower = means - (std_multiplier * stds)
+    ic_acc = (ci_lower <= y) * (ci_upper >= y)
+    ic_acc = ic_acc.float().mean()
+    return ic_acc, (ci_upper >= y).float().mean(), (ci_lower <= y).float().mean()
+
+
+regressor = BayesianRegressor(13, 1)
+optimizer = optim.SGD(regressor.parameters(), lr=0.001)
+criterion = torch.nn.MSELoss()
+
+ds_train = torch.utils.data.TensorDataset(X_train, y_train)
+dataloader_train = torch.utils.data.DataLoader(ds_train, batch_size=16, shuffle=True)
+
+ds_test = torch.utils.data.TensorDataset(X_test, y_test)
+dataloader_test = torch.utils.data.DataLoader(ds_test, batch_size=16, shuffle=True)
+
+
+iteration = 0
+for epoch in range(100):
+    for i, (datapoints, labels) in enumerate(dataloader_train):
+        optimizer.zero_grad()
+        
+        loss = regressor.sample_elbo(inputs=datapoints,
+                           labels=labels,
+                           criterion=criterion,
+                           sample_nbr=3)
+        loss.backward()
+        optimizer.step()
+        
+        iteration += 1
+        if iteration%100==0:
+            ic_acc, under_ci_upper, over_ci_lower = evaluate_regression(regressor,
+                                                                        X_test,
+                                                                        y_test,
+                                                                        samples=25,
+                                                                        std_multiplier=3)
+            
+            print("CI acc: {:.2f}, CI upper acc: {:.2f}, CI lower acc: {:.2f}".format(ic_acc, under_ci_upper, over_ci_lower))
+            print("Loss: {:.4f}".format(loss))
