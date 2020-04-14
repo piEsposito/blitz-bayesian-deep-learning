2912bc78ef4cc2d3da356e6beb6e057032a52188
u29446 <piero.skywalker@gmail.com>
2020-04-14 09:24:06 -0700

    variational lstm inference tests passing

--- blitz/modules/tests/lstm_bayesian_layer_test.py
+++ blitz/modules/tests/lstm_bayesian_layer_test.py
@@ -13,9 +13,30 @@ class TestLinearBayesian(unittest.TestCase):
         pass
     
     def test_infer_shape_1_sample(self):
-        deterministic_lstm = nn.LSTM(1, 100, 1, batch_first=True)
+        deterministic_lstm = nn.LSTM(1, 10, 1, batch_first=True)
         in_data = torch.ones((10, 10, 1))
         det_inference = deterministic_lstm(in_data)
+        
+        b_lstm = BayesianLSTM(1, 10)
+        b_inference = b_lstm(in_data, hidden_states=None)
+        
+        self.assertEqual(det_inference[0].shape, b_inference[0].shape)
+        pass
+    
+    def test_variational_inference(self):
+        #create module, check if inference is variating
+        deterministic_lstm = nn.LSTM(1, 10, 1, batch_first=True)
+        in_data = torch.ones((10, 10, 1))
+        det_inference = deterministic_lstm(in_data)
+        
+        b_lstm = BayesianLSTM(1, 10)
+        b_inference_1 = b_lstm(in_data, hidden_states=None)
+        b_inference_2 = b_lstm(in_data, hidden_states=None)
+        
+        self.assertEqual((b_inference_1[0] != b_inference_2[0]).any(), torch.tensor(True))
+        self.assertEqual((det_inference[0] == det_inference[0]).all(), torch.tensor(True))
+        pass
+        
     
 if __name__ == "__main__":
     unittest.main()
