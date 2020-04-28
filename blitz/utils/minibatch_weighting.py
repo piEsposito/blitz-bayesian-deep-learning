3b66001efd02042948a4a3ead984f2f500ceca70
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-04-28 15:33:08 +0100

    Provided minibatch weighting
    Provided an implementation of minibatch weighting as described in section 3..4 of the [Weight Uncertainty in Neural Networks](https://arxiv.org/pdf/1505.05424.pdf) paper.
    
    Resolves: #11

---
+++ blitz/utils/minibatch_weighting.py
@@ -0,0 +1,16 @@
+def minibatch_weight(batch_idx, num_batches):
+
+    """Calculates the minibatch weight.
+
+        A formula for calculating the minibatch weight is described in
+        section 3.4 of the 'Weight Uncertainty in Neural Networks' paper.
+        The weighting decreases as the batch index increases, this is
+        because the the first few batches are influenced heavily by
+        the complexity cost.
+
+    Parameters:
+        batch_idx: int -> the current batch index
+        num_batches: int -> the total number of batches
+    """
+
+    return 2 ^ (num_batches - batch_idx) / (2 ^ num_batches - batch_idx)
