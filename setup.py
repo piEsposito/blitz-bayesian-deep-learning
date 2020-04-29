c4b222826de8da65711fdeab58dd94b919fc19b7
piEsposito <piero.esposito@intel.com>
2020-04-29 15:48:01 -0700

    uploaded to pypi

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open(path.join(this_directory, 'requirements.txt'), encoding='utf-8') as f:
     install_requires = f.read()
 
 setup(
-    name = "blitz-bayesian-torch",
+    name = "blitz-bayesian-pytorch",
     packages = find_packages(),
-    version = "0.1.1",
+    version = "0.2",
     description = "A simple and extensible library to create Bayesian Neural Network Layers on PyTorch without trouble and with full integration with nn.Module and nn.Sequential.",
     author = "Pi Esposito",
     url = "https://github.com/piEsposito/blitz-bayesian-deep-learning",
-    long_desc = long_desc,
+    long_description = long_desc,
     long_description_content_type = "text/markdown",
     install_requires = install_requires,
     classifiers = [
