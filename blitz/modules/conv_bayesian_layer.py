b1d896c8e98d5f9c9f0a7a37f96dd654e4ed420f
Pi Esposito <piero.skywalker@gmail.com>
2020-05-18 13:24:48 -0300

    feature: conv1d layer

--- blitz/modules/conv_bayesian_layer.py
+++ blitz/modules/conv_bayesian_layer.py
@@ -6,6 +6,132 @@ from torch.nn import functional as F
 from blitz.modules.base_bayesian_module import BayesianModule
 from blitz.modules.weight_sampler import GaussianVariational, ScaleMixturePrior
 
+class BayesianConv1d(BayesianModule):
+
+    # Implements Bayesian Conv2d layer, by drawing them using Weight Uncertanity on Neural Networks algorithm
+    """
+    Bayesian Linear layer, implements a Convolution 2D layer as proposed on Weight Uncertainity on Neural Networks
+    (Bayes by Backprop paper).
+
+    Its objective is be interactable with torch nn.Module API, being able even to be chained in nn.Sequential models with other non-this-lib layers
+
+    parameters:
+        in_channels: int -> incoming channels for the layer
+        out_channels: int -> output channels for the layer
+        kernel_size : tuple (int, int) -> size of the kernels for this convolution layer
+        groups : int -> number of groups on which the convolutions will happend
+        padding : int -> size of padding (0 if no padding)
+        dilation int -> dilation of the weights applied on the input tensor
+
+
+        bias: bool -> whether the bias will exist (True) or set to zero (False)
+        prior_sigma_1: float -> prior sigma on the mixture prior distribution 1
+        prior_sigma_2: float -> prior sigma on the mixture prior distribution 2
+        prior_pi: float -> pi on the scaled mixture prior
+        posterior_mu_init float -> posterior mean for the weight mu init
+        posterior_rho_init float -> posterior mean for the weight rho init
+        freeze: bool -> wheter the model will start with frozen(deterministic) weights, or not
+    
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 groups = 1,
+                 stride = 1,
+                 padding = 0,
+                 dilation = 1,
+                 bias=True,
+                 prior_sigma_1 = 0.1,
+                 prior_sigma_2 = 0.002,
+                 prior_pi = 1,
+                 posterior_mu_init = 0,
+                 posterior_rho_init = -6.0,
+                 freeze = False):
+        super().__init__()
+        
+        #our main parameters
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.freeze = freeze
+        self.kernel_size = kernel_size
+        self.groups = groups
+        self.stride = stride
+        self.padding = padding
+        self.dilation = dilation
+        self.bias = bias
+
+
+        self.posterior_mu_init = posterior_mu_init
+        self.posterior_rho_init = posterior_rho_init
+
+        #parameters for the scale mixture prior
+        self.prior_sigma_1 = prior_sigma_1
+        self.prior_sigma_2 = prior_sigma_2
+        self.prior_pi = prior_pi
+
+        #our weights
+        self.weight_mu = nn.Parameter(torch.Tensor(out_channels, in_channels // groups, kernel_size).normal_(posterior_mu_init, 0.1))
+        self.weight_rho = nn.Parameter(torch.Tensor(out_channels, in_channels // groups, kernel_size).normal_(posterior_rho_init, 0.1))
+        self.weight_sampler = GaussianVariational(self.weight_mu, self.weight_rho)
+
+        #our biases
+        self.bias_mu = nn.Parameter(torch.Tensor(out_channels).normal_(posterior_mu_init, 0.1))
+        self.bias_rho = nn.Parameter(torch.Tensor(out_channels).normal_(posterior_rho_init, 0.1))
+        self.bias_sampler = GaussianVariational(self.bias_mu, self.bias_rho)
+
+        # Priors (as BBP paper)
+        self.weight_prior_dist = ScaleMixturePrior(self.prior_pi, self.prior_sigma_1, self.prior_sigma_2)
+        self.bias_prior_dist = ScaleMixturePrior(self.prior_pi, self.prior_sigma_1, self.prior_sigma_2)
+        self.log_prior = 0
+        self.log_variational_posterior = 0
+
+    def forward(self, x):
+        #Forward with uncertain weights, fills bias with zeros if layer has no bias
+        #Also calculates the complecity cost for this sampling
+        if self.freeze:
+            return self.forward_frozen(x)
+
+        w = self.weight_sampler.sample()
+
+        if self.bias:
+            b = self.bias_sampler.sample()
+            b_log_posterior = self.bias_sampler.log_posterior()
+            b_log_prior = self.bias_prior_dist.log_prior(b)
+
+        else:
+            b = torch.zeros((self.out_channels))
+            b_log_posterior = 0
+            b_log_prior = 0
+
+        self.log_variational_posterior = self.weight_sampler.log_posterior() + b_log_posterior
+        self.log_prior = self.weight_prior_dist.log_prior(w) + b_log_prior
+
+        return F.conv1d(input=x,
+                        weight=w,
+                        bias=b,
+                        stride=self.stride,
+                        padding=self.padding,
+                        dilation=self.dilation,
+                        groups=self.groups)
+
+    def forward_frozen(self, x):
+        # Computes the feedforward operation with the expected value for weight and biases (frozen-like)
+
+        if self.bias:
+            bias = self.bias_mu
+            assert bias is self.bias_mu, "The bias inputed should be this layer parameter, not a clone."
+        else:
+            bias = torch.zeros(self.out_channels)
+
+        return F.conv1d(input=x,
+                        weight=self.weight_mu,
+                        bias=bias,
+                        stride=self.stride,
+                        padding=self.padding,
+                        dilation=self.dilation,
+                        groups=self.groups)
+
 class BayesianConv2d(BayesianModule):
 
     # Implements Bayesian Conv2d layer, by drawing them using Weight Uncertanity on Neural Networks algorithm
--- blitz/modules/tests/conv_bayesian_layer_test.py
+++ blitz/modules/tests/conv_bayesian_layer_test.py
@@ -1,9 +1,116 @@
 import unittest
-from blitz.modules import BayesianConv2d
+from blitz.modules import BayesianConv2d, BayesianConv1d
 from blitz.modules.base_bayesian_module import BayesianModule
 
 import torch
 from torch import nn
+
+class TestConv1DBayesian(unittest.TestCase):
+
+    def test_init_bayesian_layer(self):
+        #create bayesian layer
+
+        module = BayesianConv1d(3, 10, 3)
+        pass
+
+    def test_weights_shape(self):
+       #check if weights shape is the expected
+       bconv = BayesianConv1d(in_channels=3,
+                              out_channels=3,
+                              kernel_size=3)
+
+       conv = nn.Conv1d(in_channels=3,
+                        out_channels=3,
+                        kernel_size=3)
+
+       to_feed = torch.ones((1, 3, 25))
+
+       infer1 = bconv(to_feed)
+       infer2 = conv(to_feed)
+       
+       self.assertEqual(infer1.shape, infer2.shape)
+       pass
+
+    def test_weights_shape_cuda(self):
+       #check if weights shape is the expected
+       device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+       bconv = BayesianConv1d(in_channels=3,
+                              out_channels=3,
+                              kernel_size=3).to(device)
+
+       conv = nn.Conv1d(in_channels=3,
+                        out_channels=3,
+                        kernel_size=3).to(device)
+
+       to_feed = torch.ones((1, 3, 25)).to(device)
+
+       infer1 = bconv(to_feed)
+       infer2 = conv(to_feed)
+       
+       self.assertEqual(infer1.shape, infer2.shape)
+       pass
+
+    def test_variational_inference(self):
+        #create module, check if inference is variating
+        bconv = BayesianConv1d(in_channels=3,
+                              out_channels=3,
+                              kernel_size=1)
+
+        conv = nn.Conv1d(in_channels=3,
+                        out_channels=3,
+                        kernel_size=3)
+
+        to_feed = torch.ones((1, 3, 25))
+
+        self.assertEqual((bconv(to_feed) != bconv(to_feed)).any(), torch.tensor(True))
+        self.assertEqual((conv(to_feed) == conv(to_feed)).all(), torch.tensor(True))
+        pass
+
+
+    def test_freeze_module(self):
+        #create module, freeze
+        #check if two inferences keep equal
+        bconv = BayesianConv1d(in_channels=3,
+                              out_channels=3,
+                              kernel_size=3,
+                              bias=False)
+
+        to_feed = torch.ones((1, 3, 25))
+
+        self.assertEqual((bconv(to_feed) != bconv(to_feed)).any(), torch.tensor(True))
+
+        frozen_feedforward = bconv.forward_frozen(to_feed)
+        bconv.freeze = True
+        self.assertEqual((bconv.forward(to_feed) == frozen_feedforward).all(), torch.tensor(True))
+        pass
+    
+    def test_inheritance(self):
+
+        #check if bayesian linear has nn.Module and BayesianModule classes
+        bconv = BayesianConv1d(in_channels=3,
+                              out_channels=3,
+                              kernel_size=3,
+                              bias=False)
+
+        self.assertEqual(isinstance(bconv, (nn.Module)), True)
+        self.assertEqual(isinstance(bconv, (BayesianModule)), True)
+
+    def test_kl_divergence(self):
+        #create model, sample weights
+        #check if kl divergence between apriori and a posteriori is working
+        bconv = BayesianConv1d(in_channels=3,
+                              out_channels=3,
+                              kernel_size=3)
+
+        to_feed = torch.ones((1, 3, 25))
+        predicted = bconv(to_feed)
+        
+        complexity_cost = bconv.log_variational_posterior - bconv.log_prior
+        self.assertEqual((complexity_cost == complexity_cost).all(), torch.tensor(True))
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
 
 class TestConv2DBayesian(unittest.TestCase):
 
