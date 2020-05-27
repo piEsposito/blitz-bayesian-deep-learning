fix: kl divergence on weight samplers

blitz/examples/bayesian_LeNet_mnist.py
@@ -60,12 +60,15 @@ for epoch in range(100):
         loss = classifier.sample_elbo(inputs=datapoints.to(device),
                            labels=labels.to(device),
                            criterion=criterion,
-                           sample_nbr=3)
+                           sample_nbr=3,
+                           complexity_cost_weight=1/50000)
+        #print(loss)
         loss.backward()
         optimizer.step()
         
         iteration += 1
-        if iteration%1000==0:
+        if iteration%250==0:
+            print(loss)
             correct = 0
             total = 0
             with torch.no_grad():

blitz/modules/weight_sampler.py
@@ -47,8 +47,8 @@ class GaussianVariational(nn.Module):
             w = self.w
         
         log_sqrt2pi = np.log(np.sqrt(2*self.pi))
-        log_posteriors =  -log_sqrt2pi - torch.log(self.sigma) - (((w - self.mu) ** 2)/(2 * self.sigma ** 2))
-        return log_posteriors.mean()
+        log_posteriors =  -log_sqrt2pi - torch.log(self.sigma) - (((w - self.mu) ** 2)/(2 * self.sigma ** 2)) - 0.5
+        return log_posteriors.sum()
 
 class ScaleMixturePrior(nn.Module):
     #Calculates a Scale Mixture Prior distribution for the prior part of the complexity cost on Bayes by Backprop paper
@@ -90,4 +90,4 @@ class ScaleMixturePrior(nn.Module):
 
         prior_pdf = (self.pi * prob_n1 + (1 - self.pi) * prob_n2)
 
-        return (torch.log(prior_pdf)).mean()
+        return (torch.log(prior_pdf) - 0.5).sum()